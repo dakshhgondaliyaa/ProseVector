d8812038e6e6ea921511f62f0b4e5cdb76704dd6
Daksh <dakshgondaliyaa11@gmail.com>
2026-08-19 23:43:59 +0530

    Remove unused test scripts

--- check.py
+++
@@ -1,5 +0,0 @@
-from datasets import load_dataset
-ds = load_dataset('textminr/cmu-book-summaries', split='train')
-titles = [item['title'] for item in ds]
-targets = ['The Art of War', 'Meditations', 'The Origin of Species', 'Utilitarianism', 'Beyond Good and Evil', 'Thus Spoke Zarathustra', 'Communist Manifesto', 'Das Kapital', 'Capital', 'Silent Spring', 'The Selfish Gene', 'Guns, Germs, and Steel', "A People's History of the United States", "The Blank Slate", "Thinking, Fast and Slow", "A Brief History of Time", "The Structure of Scientific Revolutions", "The Interpretation of Dreams", "Orientalism"]
-print([t for t in targets if t in titles])
